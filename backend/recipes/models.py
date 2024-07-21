a53b9870cd8c4d388967a3a06d1deb388c5a3ccf
Ричард <ququssa@mail.ru>
2024-07-21 21:16:19 +0300

    Попытка_№2002

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -203,56 +203,59 @@ class RecipeIngredient(models.Model):
         )
 
 
-class UserRecipeRelation(models.Model):
+class Favorite(models.Model):
     user = models.ForeignKey(
         User,
-        verbose_name='Пользователь',
-        related_name='favorites_user',
-        help_text='Пользователь',
         on_delete=models.CASCADE,
+        related_name="favorite",
+        verbose_name="Пользователь",
     )
     recipe = models.ForeignKey(
         Recipe,
-        verbose_name='Рецепт',
-        related_name='favorites_recipe',
-        help_text='Рецепт',
         on_delete=models.CASCADE,
+        related_name="favorite",
+        verbose_name="Рецепт",
     )
 
     class Meta:
-        abstract = True
+        verbose_name = "Избранное"
+        verbose_name_plural = "Избранное"
+
         constraints = (
             models.UniqueConstraint(
-                fields=('user', 'recipe'),
-                name=('\n%(app_label)s_%(class)s recipe is'
-                      ' already related to user\n'),
+                fields=("user", "recipe"), name="unique_favorite_recipe"
             ),
         )
 
+    def __str__(self):
+        return f"Рецепт {self.recipe} в избранном у пользователя {self.user}"
 
-class Favorite(UserRecipeRelation):
-    """Модель избранных рецептов"""
 
-    date_added = models.DateTimeField(
-        verbose_name='Дата добавления',
-        auto_now_add=True,
-        editable=False,
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="shopping_list",
+        verbose_name="Пользователь",
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name="shopping_list",
+        verbose_name="Рецепт",
     )
 
-    class Meta(UserRecipeRelation.Meta):
-        verbose_name = 'Избранный рецепт'
-        verbose_name_plural = 'Избранные рецепты'
+    class Meta:
+        verbose_name = "Список покупок"
+        verbose_name_plural = "Список покупок"
+
+        constraints = (
+            models.UniqueConstraint(
+                fields=("user", "recipe"), name="unique_shopping_list_recipe"
+            ),
+        )
 
     def __str__(self):
-        return f'{self.user} добавил "{self.recipe}" в Избранное'
-
-
-class ShoppingCart(UserRecipeRelation):
-    """ Модель Корзина покупок """
-
-    class Meta(UserRecipeRelation.Meta):
-        verbose_name = 'Корзина покупок'
-        verbose_name_plural = 'Корзина покупок'
-
-    def __str__(self):
-        return f'{self.user} добавил "{self.recipe}" в Корзину покупок'
+        return (
+            f"Рецепт {self.recipe} в списке покупок у пользователя {self.user}"
+        )
