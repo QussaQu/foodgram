67c0c480d8fc1b605886163b69d1efc104643d3a
Ричард <ququssa@mail.ru>
2024-07-31 18:23:08 +0300

    Try_to_fix_all_5

--- backend/users/models.py
+++ backend/users/models.py
@@ -52,4 +52,3 @@ class Subscribe(models.Model):
         ]
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
-
--- backend/users/views.py
+++ backend/users/views.py
@@ -10,7 +10,6 @@ from rest_framework.response import Response
 from users.models import Subscribe, User
 
 
-
 class NewUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = NewUserSerializer
