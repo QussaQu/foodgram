3d86a18f5fc505e5609ebc8183d6b0e5a0e3b7e9
Ричард <ququssa@mail.ru>
2024-08-01 12:52:55 +0300

    Try_to_fix_all_25

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,6 @@
+from colorfield.fields import ColorField
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator, RegexValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models import UniqueConstraint
 
@@ -26,16 +27,10 @@ class Tag(models.Model):
     """ Модель Тэг """
 
     name = models.CharField('Название', unique=True, max_length=200)
-    color = models.CharField(
+    color = ColorField(
         'Цветовой HEX-код',
         unique=True,
-        max_length=7,
-        validators=[
-            RegexValidator(
-                regex='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
-                message='Введенное значение не является цветом в формате HEX!'
-            )
-        ]
+        max_length=7
     )
     slug = models.SlugField('Уникальный слаг', unique=True, max_length=200)
 
