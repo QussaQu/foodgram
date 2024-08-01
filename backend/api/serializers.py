a1b336e0d2755bfc185cf5aa3d3a81fd08b93c32
Ричард <ququssa@mail.ru>
2024-08-01 16:50:53 +0300

    Try_to_fix_all_33

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -196,8 +196,8 @@ class RecipeWriteSerializer(ModelSerializer):
             raise ValidationError({'errors': 'Рецепт уже добавлен!'})
         return data
 
-    def validate_tags(self, value):
-        tags = value
+    def validate_tags(self, data):
+        tags = data.get('tags')
         if not tags:
             raise ValidationError({'tags': 'Нужно выбрать хотя бы один тег!'})
         tags_list = []
@@ -207,7 +207,7 @@ class RecipeWriteSerializer(ModelSerializer):
                     {'tags': 'Теги должны быть уникальными!'}
                 )
             tags_list.append(tag)
-        return value
+        return data
 
     @transaction.atomic
     def create_ingredients_amounts(self, ingredients, recipe):
