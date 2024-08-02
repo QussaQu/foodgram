877d7dc162b5dd8154e3b91ce9dbb838bd8ce727
Ричард <ququssa@mail.ru>
2024-08-02 19:25:53 +0300

    Try_to_fix_all_50

--- backend/api/views.py
+++ backend/api/views.py
@@ -73,7 +73,7 @@ class RecipeViewSet(ModelViewSet):
                 default=Value(False),
                 output_field=BooleanField()
             )
-        ).select_related('author').prefetch_related('tags', 'ingredient')
+        ).select_related('author').prefetch_related('tags', 'ingredient_list')
 #                                                                   ^ тут
 
     def get_serializer_class(self):
