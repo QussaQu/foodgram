98c7c8a1cf23dca9e687400af070d9e3bbf227b0
Ричард <ququssa@mail.ru>
2024-08-11 23:38:04 +0300

    Доработка сериализатора (13).

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,7 +3,6 @@ from django.db import transaction
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 from rest_framework.fields import SerializerMethodField
 from rest_framework.validators import UniqueTogetherValidator
 from rest_framework.serializers import ModelSerializer, BooleanField
